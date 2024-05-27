5fc55af3d77899237eb97b4a58d69d5da21e414b
Bram de Vries <acdevries@home.nl>
2024-05-27 14:02:02 +0200

    Load libml from pypi and fix DVC pipeline

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -12,32 +12,27 @@ def preprocess_data():
     raw_val = pd.read_csv('./data/raw/val.csv', dtype='string')
     raw_test = pd.read_csv('./data/raw/test.csv', dtype='string')
 
-    (
-        char_index,
-        x_train,
-        x_val,
-        x_test,
-        y_train,
-        y_val,
-        y_test
-    ) = preprocessing.process_data(raw_train, raw_val, raw_test)
+    
+    preprocessed = preprocessing.process_data(raw_train, raw_val, raw_test)
 
     with open('./data/processed/char_index.pkl', 'wb') as f:
-        pickle.dump(char_index, f)
+        pickle.dump(preprocessed['char_index'], f)
+    with open('./data/processed/tokenizer.pkl', 'wb') as f:
+        pickle.dump(preprocessed['tokenizer'], f)
 
     with open('./data/processed/x_train.pkl', 'wb') as f:
-        pickle.dump(x_train, f)
+        pickle.dump(preprocessed['x_train'], f)
     with open('./data/processed/x_val.pkl', 'wb') as f:
-        pickle.dump(x_val, f)
+        pickle.dump(preprocessed['x_val'], f)
     with open('./data/processed/x_test.pkl', 'wb') as f:
-        pickle.dump(x_test, f)
+        pickle.dump(preprocessed['x_test'], f)
 
     with open('./data/processed/y_train.pkl', 'wb') as f:
-        pickle.dump(y_train, f)
+        pickle.dump(preprocessed['y_train'], f)
     with open('./data/processed/y_val.pkl', 'wb') as f:
-        pickle.dump(y_val, f)
+        pickle.dump(preprocessed['y_val'], f)
     with open('./data/processed/y_test.pkl', 'wb') as f:
-        pickle.dump(y_test, f)
+        pickle.dump(preprocessed['y_test'], f)
 
 if __name__ == "__main__":
     preprocess_data()
