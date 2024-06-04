32e5dd35fdddf2b3328e7d60661c46a334e100ea
Bram de Vries <acdevries@home.nl>
2024-06-04 14:59:06 +0200

    Update benchmark test

--- tests/benchmark/bench.py
+++ tests/benchmark/bench.py
@@ -4,4 +4,4 @@ from models.create_model import create_model
 
 # Monitoring Tests for ML -> Test for dramatic or slow-leak regressions in training speed, serving latency, throughput, or RAM usage.
 def test_training_time(benchmark):
-    benchmark(create_model(42, 0.1))
+    benchmark(create_model, seed=42, sample_percentage=0.1)
